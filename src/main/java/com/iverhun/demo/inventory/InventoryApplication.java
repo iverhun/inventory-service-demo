2154b3a89dd4e5d78dbabca5efd3e9d453c59398
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2017-11-24 20:47:24 +0200

    simple oauth2 config

--- src/main/java/com/iverhun/demo/inventory/InventoryApplication.java
+++ src/main/java/com/iverhun/demo/inventory/InventoryApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import javax.annotation.PostConstruct;
 
@@ -15,6 +17,8 @@ import javax.annotation.PostConstruct;
         basePackageClasses = {InventoryApplication.class, Jsr310JpaConverters.class}
 )
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
 public class InventoryApplication {
 
     @Autowired
